a77a18007bee3c8ca6243a0d2235f5a7c6341a68
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2023-02-01 13:44:45 +0100

    initial

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+# Use an official Ubuntu image as the base image
+FROM ubuntu:20.04
+
+# Install dependencies
+RUN apt-get update && \ 
+apt-get install software-properties-common wget -y
+
+RUN add-apt-repository -y ppa:ethereum/ethereum && \
+apt-get update && \
+apt-get install ethereum -y && \
+apt-get upgrade geth
+
+COPY brise_mainnet.json /tmp/brise_mainnet.json
+COPY static-nodes.json /tmp/static-nodes.json
+COPY init.sh /init.sh
+RUN chmod 755 /init.sh  
+
+VOLUME /root
+
+ENTRYPOINT ["/bin/sh", "-c" , "/init.sh"]
---
+++ init.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Setup
+if [[ ! -d /root/bitgert ]]; then
+  mkdir -p /root/bitgert
+  cd /root/bitgert
+  #Download the required JSON files
+  cp /tmp/brise_mainnet.json /root/bitgert/brise_mainnet.json
+fi
+#Initialize the node
+if  [[ ! -f /root/bitgert/geth/static-nodes.json ]]; then
+  geth --datadir /root/bitgert init brise_mainnet.json
+  cp /tmp/static-nodes.json /root/bitgert/geth/static-nodes.json
+fi
+cd /root/bitgert
+#Run geth
+geth --datadir /root/bitgert --syncmode 'full' --gcmode=archive --port 40605 --http --http.port 3545 --http.api 'personal,eth,net,web3,personal,admin,miner,txpool,debug' --bootnodes enode://bab5d43f3ad9612f909547fa754b955c0509a11a55c7f067b5bdc434b5fb6cea363c193c6c00557bc90267da7aef3a5997c376754f6df2d6817cf52c90d1852e@54.251.84.204:40605 --networkid 32520
+
+
+
+
+
+
+
+
