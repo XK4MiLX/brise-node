3855cb398efd4a09b6e308d772831950f28cba39
Kyuubi2709 <89035963+Kyuubi2709@users.noreply.github.com>
2023-02-03 14:19:28 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -14,7 +14,7 @@ if  [[ ! -f /root/bitgert/geth/static-nodes.json ]]; then
 fi
 cd /root/bitgert
 #Run geth
-geth --datadir /root/bitgert --syncmode 'full' --gcmode=archive --port 40605 --http --http.port 3545 --http.api 'personal,eth,net,web3,personal,admin,miner,txpool,debug' --bootnodes enode://bab5d43f3ad9612f909547fa754b955c0509a11a55c7f067b5bdc434b5fb6cea363c193c6c00557bc90267da7aef3a5997c376754f6df2d6817cf52c90d1852e@54.251.84.204:40605 --networkid 32520
+geth --datadir /root/bitgert --syncmode 'full' --gcmode=archive --port 40605 --http --http.port 3545 --http.addr=0.0.0.0 --http.api 'personal,eth,net,web3,personal,admin,miner,txpool,debug' --bootnodes enode://bab5d43f3ad9612f909547fa754b955c0509a11a55c7f067b5bdc434b5fb6cea363c193c6c00557bc90267da7aef3a5997c376754f6df2d6817cf52c90d1852e@54.251.84.204:40605 --networkid 32520
 
 
 
